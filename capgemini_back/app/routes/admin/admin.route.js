f6eec625542e87365574859a921cd8036c8671ff
xavier-green <xavier.green@student.ecp.fr>
2017-03-07 16:45:25 +0100

    adding authentication on admin page

--- capgemini_back/app/routes/admin/admin.route.js
+++ capgemini_back/app/routes/admin/admin.route.js
@@ -4,15 +4,34 @@ const router = express.Router(); // eslint-disable-line new-cap
 
 router.route('/')
     .get((req,res,next) => {
-    	return adminController.list()
-    	.then((statsObject) => {
-    		let stats = statsObject.stats
-    		let percentages = statsObject.percentages
-    		res.render('admin',{stats,percentages});
-    	})
-    	.catch((e) => {
-    		res.json(e)
-    	})
+    	if (req.session.loggedIn) {
+    		return adminController.list()
+	    	.then((statsObject) => {
+	    		let stats = statsObject.stats
+	    		let percentages = statsObject.percentages
+	    		res.render('admin',{stats,percentages});
+	    	})
+	    	.catch((e) => {
+	    		res.json(e)
+	    	})
+    	} else {
+    		res.render('error');
+    	}
     });
+
+router.route('/login')
+	.get((req,res,next) => {
+		res.render('login');
+	})
+	.post((req,res,next) => {
+		let username = req.body.login
+		let password = req.body.pass
+		if (username=="admin" && password=="admin") {
+			req.session.loggedIn = true;
+			res.redirect('/admin')
+		} else {
+			res.render('login',{error:"Les identifiants sont faux"})
+		}
+	})
 
 module.exports = exports = router;
