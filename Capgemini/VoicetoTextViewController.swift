5e2814566c3c0664b4280fbbc8cd4cb03b393b8f
Younes Belkouchi <younes.belkouchi@student.ecp.fr>
2017-03-13 10:36:41 +0100

    Implement table view and record button on VoiceToText Screen

--- Capgemini/VoicetoTextViewController.swift
+++ Capgemini/VoicetoTextViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class VoicetoTextViewController: UIViewController {
 
+    @IBOutlet weak var recordedText: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +32,23 @@ class VoicetoTextViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return GlobalVariables.pieuvreUsernames.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as? nameCell else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+        cell.nameLabel.text = GlobalVariables.pieuvreUsernames[indexPath.row]
+        return cell
+    }
 
+}
+
+class nameCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
 }
